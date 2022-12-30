bcb4463915e38417132dabbb0ff93447a6e30c09
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2022-12-30 13:07:23 +0000

    JS_first

---
+++ app.js
@@ -0,0 +1,24 @@
+//let hello = "Hello World";
+//alert(hello);
+
+ // let orange = 100;
+ // let apple = 120;
+ // if(orange < apple){
+ //   alert("みかんの値段がりんごより安い");
+ //  }else if(orange == apple){
+ //   alert("みかんとりんごが同じ値段");
+ //  }else{
+ //   alert("みかんの値段がりんごより高い");
+ //  }
+ 
+//  let max = 100;
+//  let num = 1;
+//  let count = 0;
+//  
+//  while(num < max){
+//    num = num * 2;
+//    count = count + 1;
+//  }
+//  
+//  alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
