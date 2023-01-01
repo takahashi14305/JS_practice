e8fa47e43220fb29183429e1f4eb995001b6c73f
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-01-01 13:24:36 +0000

    JS_update

--- app.js
+++ app.js
@@ -10,15 +10,34 @@
  //  }else{
  //   alert("みかんの値段がりんごより高い");
  //  }
- 
+
 //  let max = 100;
 //  let num = 1;
 //  let count = 0;
-//  
+//
 //  while(num < max){
 //    num = num * 2;
 //    count = count + 1;
 //  }
-//  
+//
 //  alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+ num = num + i;
+}
+alert("1から10まで足し算した結果は"+ num +"です");
+
+// 整数を代入する
+let int1 = 1;
+
+// 負数を代入する
+let int2 = -10;
+
+// 小数点を代入する
+let float1 = 3.14;
+
+// 文字列を代入する
+let str1 = 'JavaScriptを覚えよう';
