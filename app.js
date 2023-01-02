1747ec960a113dd607d199890615084001d52be8
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-01-02 09:55:14 +0000

    JS_update

--- app.js
+++ app.js
@@ -10,6 +10,16 @@
  //  }else{
  //   alert("みかんの値段がりんごより高い");
  //  }
+ 
+  // let orange = 100;
+ // let apple = 120;
+ // if(orange < apple){
+ //   alert("みかんの値段がりんごより安い");
+ //  }else if(orange == apple){
+ //   alert("みかんとりんごが同じ値段");
+ //  }else{
+ //   alert("みかんの値段がりんごより高い");
+ //  }
 
 //  let max = 100;
 //  let num = 1;
