d4de5a6b6afd2506bef20115e66c790336503490
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-03-04 08:38:41 +0000

    if practice

--- app.js
+++ app.js
@@ -75,6 +75,16 @@
  //  }else{
  //   alert("みかんの値段がりんごより高い");
  //  }
+ 
+   // let orange = 100;
+ // let apple = 120;
+ // if(orange < apple){
+ //   alert("みかんの値段がりんごより安い");
+ //  }else if(orange == apple){
+ //   alert("みかんとりんごが同じ値段");
+ //  }else{
+ //   alert("みかんの値段がりんごより高い");
+ //  }
 
 
 //let orange = 120;
