c54466b2016d47c87d1f83c76d97dce3c33023e6
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-03-06 09:56:44 +0000

    if practice

--- app.js
+++ app.js
@@ -75,7 +75,7 @@
  //  }else{
  //   alert("みかんの値段がりんごより高い");
  //  }
- 
+
    // let orange = 100;
  // let apple = 120;
  // if(orange < apple){
@@ -107,3 +107,21 @@
 // alert('みかんの値段がりんごより高い')
 //}
 
+//let max = 100;
+//let num = 1;
+//let count = 0;
+//
+//while(num < max){
+// num = num * 2;
+// count = count + 1;
+//}
+//
+//alert('2を掛け続けて' + max + 'を超えるのにかかった回数は' + count + '回です')
+
+let i;
+let num = 0;
+
+for(i=1;i < 11;i++){
+ num = num + 1;
+}
+alert('1から10まで足し算した結果は' + num + 'です');
