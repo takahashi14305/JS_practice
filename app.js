0f95d2f13533f6057883f25f234ba2e813464075
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-03-15 13:18:25 +0000

    関数JS

--- app.js
+++ app.js
@@ -217,16 +217,20 @@
 //}
 
 // メイン部分
-let alertString;
+//let alertString;
+//
+//// 作成した関数を呼び出し、変数へ格納
+//alertString = addString("WebCamp");
+//
+////変数の中身をアラートで表示する
+//alert(alertString);
+//
+//// 作成した関数
+//function addString(strA){
+//  let addStr = "Hello " + strA;
+//  return addStr;
+//}
 
-// 作成した関数を呼び出し、変数へ格納
-alertString = addString("WebCamp");
+let promptStr = prompt('何か好きな文字を入力してください。');
 
-//変数の中身をアラートで表示する
-alert(alertString);
-
-// 作成した関数
-function addString(strA){
-  let addStr = "Hello " + strA;
-  return addStr;
-}
+alert(promptStr);
