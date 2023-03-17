bc6db1186f20af38410144b90de7c5dc80974f31
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-03-17 12:48:20 +0000

    関数JS

--- app.js
+++ app.js
@@ -239,3 +239,16 @@
 //
 //alert('あなたの選んだ手は' + user_hand + 'です。');
 
+unction getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+
+  if(js_hand_num == 0){
+    js_hand = "グー";
+  } else if(js_hand_num == 1){
+    js_hand = "チョキ";
+  } else if(js_hand_num == 2){
+    js_hand = "パー";
+  }
+
+  return js_hand;
+}
