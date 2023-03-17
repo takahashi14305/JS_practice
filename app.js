08983e7eac630788f70dd4cf3b37fcf60727b54a
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-03-17 12:47:54 +0000

    関数JS

--- app.js
+++ app.js
@@ -235,6 +235,7 @@
 //
 //alert(promptStr);
 
-let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+//let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+//
+//alert('あなたの選んだ手は' + user_hand + 'です。');
 
-alert('あなたの選んだ手は' + user_hand + 'です。');
